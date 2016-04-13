13926be6824ecd872fa63380a544deab437baba7
Kate Sowles <katesowles@gmail.com>
2016-04-12 20:39:55 -0700

    last changes of the night

--- js/app.js
+++ js/app.js
@@ -2,17 +2,14 @@
 $(document).ready(function() {
     $("#toPortfolio").click(function() {
         $("#about").hide();
-        $("#contact").hide();
         $("#myCarousel").show();
     });
     $("#toAbout").click(function() {
         $("#myCarousel").hide();
-        $("#contact").hide();
         $("#about").show();
     });
     $("#toContact").click(function() {
         $("#myCarousel").hide();
         $("#about").hide();
-        $("#contact").show();
     });
 });

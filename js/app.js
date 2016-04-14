c3a0cdd34d67b5fcd34b3999bfae975557e8f35c
Kate Sowles <katesowles@gmail.com>
2016-04-14 15:14:28 -0700

    pushing up the requirements for today before tinkering morea

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // hides or shows content based on nav clicks
 $(document).ready(function() {
-    $("#toPortfolio").click(function() {
-        $("#about").hide();
-        $("#myCarousel").show();
-    });
-    $("#toAbout").click(function() {
-        $("#myCarousel").hide();
-        $("#about").show();
-    });
+  $('#toPortfolio').click(function() {
+    $('#about').hide();
+    $('#myCarousel').show();
+  });
+  $('#toAbout').click(function() {
+    $('#myCarousel').hide();
+    $('#about').show();
+  });
 });

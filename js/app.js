fdb14cfcefabad912670dbb774bc19220db7594f
Kate Sowles <katesowles@gmail.com>
2016-04-19 20:27:13 -0700

    final changes for the night; using .map, .reduce, IIFEs, and templates where possible.

--- js/app.js
+++ js/app.js
@@ -1,90 +1,84 @@
-function Project (opts) {
-  // for (key in opts) this[key] = opts[key];
-  this.title = opts.title;
-  this.category = opts.category;
-  this.clientUrl = opts.clientUrl;
-  this.imageSrc = opts.imageSrc;
-  this.description = opts.description;
-  this.publishedOn = opts.publishedOn;
-};
+(function(module) {
 
-Project.all = [];
+  function Project (opts) {
+    this.title = opts.title;
+    this.category = opts.category;
+    this.clientUrl = opts.clientUrl;
+    this.imageSrc = opts.imageSrc;
+    this.description = opts.description;
+    this.publishedOn = opts.publishedOn;
+  };
 
-Project.prototype.toHtml = function() {
-  var source = $('#projectTemplate').html();
-  var template = Handlebars.compile(source);
+  Project.all = [];
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  Project.prototype.toHtml = function() {
+    var source = $('#projectTemplate').html();
+    var template = Handlebars.compile(source);
 
-  return template(this);
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-};
+    return template(this);
+  };
 
-Project.loadAndSort = function(rawData) {
-  // sorts the data in order of published on, most recent at top
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  // pushes each completed project to the project array
-  rawData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function(rawData) {
-  if (localStorage.rawData) {
-    Project.loadAndSort(JSON.parse(localStorage.rawData));
-    projectView.buildIndexPage();
-  } else {
-    var newData = $.getJSON( 'js/portfolioItems.json' );
-    newData.done(function (data) {
-      Project.loadAndSort(data);
-      localStorage.rawData = JSON.stringify(data);
-      projectView.buildIndexPage();
+  Project.loadAndSort = function(rawData) {
+    // sorts the data in order of published on, most recent at top
+    rawData.reduce(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     });
-  }
-};
 
-////////////////////////////////////////////////////////////////////////////////
+    // loads the now-sorted data via .map()
+    Project.all = rawData.map(function(item) {
+      return new Project(item);
+    });
+  };
 
-var projectView = {};
+  Project.fetchAll = function(rawData) {
+    // if the data is already in localStorage, parse it and pull it back
+    if (localStorage.rawData) {
+      Project.loadAndSort(JSON.parse(localStorage.rawData));
+      projectView.buildIndexPage();
+    } else {
+    // if the data isn't in localStorage, stringify and put it there.
+      var newData = $.getJSON( 'js/portfolioItems.json' );
+      newData.done(function (data) {
+        Project.loadAndSort(data);
+        localStorage.rawData = JSON.stringify(data);
+        projectView.buildIndexPage();
+      });
+    }
+  };
 
-projectView.buildIndexPage = function () {
-  // removes the active status from all but the first project slide
-  $('.carousel-inner').find(function() {
-    $('.active:gt(1)').removeClass('active');
+  ////////////////////////////////////////////////////////////////////////////////
+
+  var projectView = {};
+
+  projectView.buildIndexPage = function () {
+    // removes the active status from all but the first project slide
+    $('.carousel-inner').find(function() {
+      $('.active:gt(1)').removeClass('active');
+    });
+
+    // pushes each project to the carousel
+    Project.all.forEach(function(a){
+      $('.carousel-inner').append(a.toHtml());
+    });
+  };
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // hides or shows content based on nav clicks
+  $(document).ready(function() {
+    $('#toPortfolio').click(function() {
+      $('#about').hide();
+      $('#myCarousel').show();
+    });
+    $('#toAbout').click(function() {
+      $('#myCarousel').hide();
+      $('#about').show();
+    });
   });
 
-  // adds a bubble nav dot for each project in the carousel
-  // $('.carousel-inner').find(function() {
-  //   rawData.forEach(function(b) {
-  //     $('ol').append('<li data-target="#myCarousel" data-slide-to="' + (b - 1) + '"></li>');
-  //   });
-  // });
+  module.Project = Project;
 
-  // removed the last, unused bubble nav bubble
-  $('.carousel-indicators').find(function() {
-    $('li:last-child').remove();
-  });
-
-  // pushes each project to the carousel
-  Project.all.forEach(function(a){
-    $('.carousel-inner').append(a.toHtml());
-  });
-};
-
-////////////////////////////////////////////////////////////////////////////////
-
-// hides or shows content based on nav clicks
-$(document).ready(function() {
-  $('#toPortfolio').click(function() {
-    $('#about').hide();
-    $('#myCarousel').show();
-  });
-  $('#toAbout').click(function() {
-    $('#myCarousel').hide();
-    $('#about').show();
-  });
-});
+})(window);
